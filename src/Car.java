0f5816fcb32dc30e37bdd3279508021f57d52695
Eugene <kolchak-zha@yandex.ru>
2023-03-08 21:49:07 +0400

    Сделано ДЗ

--- src/Car.java
+++ src/Car.java
@@ -1,48 +1,140 @@
+import java.util.Objects;
+
 public class Car {
-    String brand;
-    String model;
-    float engineVolume;
-    String color;
-    int productionYear;
-    String productionCountry;
+    private final String brand;
+    private final String model;
+    private float engineVolume;
+    private String color;
+    private final int productionYear;
+    private final String productionCountry;
+    private String transmission;
+    private final String bodyType;
+    private String registrationNumber;
+    private final int seatsNumber;
+    private boolean winterTires;
 
-    Car() {
-
+    public String getBrand() {
+        return brand;
     }
 
-    Car(String brand, String model, float engineVolume, String color, int productionYear, String productionCountry) {
-        if(brand != null) {
-            this.brand = brand;
-        } else {
-            this.brand = "default";
-        }
-        if(model != null) {
-            this.model = model;
-        } else {
-            this.model = "default";
-        }
+    public String getModel() {
+        return model;
+    }
+
+    public float getEngineVolume() {
+        return engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public String getProductionCountry() {
+        return productionCountry;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public int getSeatsNumber() {
+        return seatsNumber;
+    }
+
+    public boolean isWinterTires() {
+        return winterTires;
+    }
+
+    public void setEngineVolume(float engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public void setWinterTires(boolean winterTires) {
+        this.winterTires = winterTires;
+    }
+
+
+    public Car(String brand, String model, float engineVolume, String color, int productionYear, String productionCountry, String transmission, String bodyType, String registrationNumber, int seatsNumber, boolean winterTires) {
+        this.brand = Objects.requireNonNullElse(brand, "default");
+        this.model = Objects.requireNonNullElse(model, "default");
         if(engineVolume >= 0) {
             this.engineVolume = engineVolume;
         } else {
             this.engineVolume = 1.5f;
         }
-        if(color != null) {
-            this.color = color;
-        } else {
-            this.color = "Белый";
-        }
+        this.color = Objects.requireNonNullElse(color, "Белый");
         if(productionYear >= 0) {
             this.productionYear = productionYear;
         } else {
             this.productionYear = 2000;
         }
-        if(productionCountry != null) {
-            this.productionCountry = productionCountry;
+        this.productionCountry = Objects.requireNonNullElse(productionCountry, "default");
+        this.transmission = Objects.requireNonNullElse(transmission, "default");
+        this.bodyType = Objects.requireNonNullElse(bodyType, "default");
+        this.registrationNumber = Objects.requireNonNullElse(registrationNumber, "х000хх000");
+        if(seatsNumber != 0) {
+            this.seatsNumber = seatsNumber;
         } else {
-            this.productionCountry = "default";
+            this.seatsNumber = 5;
         }
+        this.winterTires = winterTires;
     }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + getBrand() + '\'' +
+                ", model='" + getModel() + '\'' +
+                ", engineVolume=" + getEngineVolume() +
+                ", color='" + getColor() + '\'' +
+                ", productionYear=" + getProductionYear() +
+                ", productionCountry='" + getProductionCountry() + '\'' +
+                ", transmission='" + getTransmission() + '\'' +
+                ", bodyType='" + getBodyType() + '\'' +
+                ", registrationNumber=" + getRegistrationNumber() +
+                ", seatsNumber=" + getSeatsNumber() +
+                ", winterTires=" + isWinterTires() +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Float.compare(car.engineVolume, engineVolume) == 0 && productionYear == car.productionYear && registrationNumber == car.registrationNumber && seatsNumber == car.seatsNumber && winterTires == car.winterTires && Objects.equals(brand, car.brand) && Objects.equals(model, car.model) && Objects.equals(color, car.color) && Objects.equals(productionCountry, car.productionCountry) && Objects.equals(transmission, car.transmission) && Objects.equals(bodyType, car.bodyType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brand, model, engineVolume, color, productionYear, productionCountry, transmission, bodyType, registrationNumber, seatsNumber, winterTires);
+    }
+
     void specifications() {
-        System.out.println("Марка: " + brand + ". Модель: " + model + ". Объем двигателя: " + engineVolume + " л. Цвет кузова: " + color + ". Год выпуска: " + productionYear + ". Страна-производитель: " + productionCountry + ".");
+        System.out.println("Марка: " + getBrand() + ". Модель: " + getModel() + ". Объем двигателя: " + getEngineVolume() + " л. Цвет кузова: " + getColor() + ". Год выпуска: " + getProductionYear() + ". Страна-производитель: " + getProductionCountry() + ". Трансмиссия: " + getTransmission() + ". Тип кузова: " + getBodyType() + ". Регистрационный номер: " + getRegistrationNumber() + ". Количество мест: " + getSeatsNumber() + ". Зимняя резина: " + isWinterTires());
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        // Домашнее задание 1
-        Car lada = new Car();
-        lada.brand = "Lada";
-        lada.model = "Granda";
-        lada.engineVolume = 1.7f;
-        lada.color = "Желтый";
-        lada.productionYear = 2015;
-        lada.productionCountry = "Россия";
-        Car audi = new Car();
-        audi.brand = "Audi";
-        audi.model = "A8 50 L TDI quattro";
-        audi.engineVolume = 3.0f;
-        audi.color = "Черный";
-        audi.productionYear = 2020;
-        audi.productionCountry = "Германия";
-        Car bmw = new Car();
-        bmw.brand = "BMW";
-        bmw.model = "Z8";
-        bmw.engineVolume = 3.0f;
-        bmw.color = "Черный";
-        bmw.productionYear = 2021;
-        bmw.productionCountry = "Германия";
-        Car kia = new Car();
-        kia.brand = "Kia";
-        kia.model = "Sportage 4-го поколения";
-        kia.engineVolume = 2.4f;
-        kia.color = "Красный";
-        kia.productionYear = 2018;
-        kia.productionCountry = "Южная Корея";
-        Car hyundai = new Car();
-        hyundai.brand = "Hyundai";
-        hyundai.model = "Avante";
-        hyundai.engineVolume = 1.6f;
-        hyundai.color = "Оранжевый";
-        hyundai.productionYear = 2016;
-        hyundai.productionCountry = "Южная Корея";
+        Car lada= new Car("Lada", "Granta", 1.7f, "Желтый", 2015, "Россия", "механическая", "седан", "в306уа18",5, false);
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", 3.0f, "Черный", 2020, "Германия", "автоматическая", "седан", "о607кт18",5, false);
+        Car bmw = new Car("BMW", "Z8", 3.0f, "Черный", 2021, "Германия", "автоматическая", "седан", "о123кт18",5, false);
+        Car kia = new Car("Kia", "Sportage 4-го поколения", 2.4f, "Красный", 2018, "Южная Корея", "автоматическая", "кроссовер", "о567кт18",7, false);
+        Car hyundai = new Car("Hyundai", "Avante", 1.6f, "Оранжевый", 2016, "Южная Корея", "автоматическая", "хетчбек", "о789кт18",5, false);
         lada.specifications();
         audi.specifications();
         bmw.specifications();
         kia.specifications();
         hyundai.specifications();
-        // Домашнее задание 2
-        Car lada2 = new Car("Lada", "Granta", 1.7f, "Желтый", 2015, "Россия");
-        Car audi2 = new Car("Audi", "A8 50 L TDI quattro", 3.0f, "Черный", 2020, "Германия");
-        Car bmw2 = new Car("BMW", "Z8", 3.0f, "Черный", 2021, "Германия");
-        Car kia2 = new Car("Kia", "Sportage 4-го поколения", 2.4f, "Красный", 2018, "Южная Корея");
-        Car hyundai2 = new Car("Hyundai", "Avante", 1.6f, "Оранжевый", 2016, "Южная Корея");
-        lada2.specifications();
-        audi2.specifications();
-        bmw2.specifications();
-        kia2.specifications();
-        hyundai2.specifications();
     }
 }
